f486715c84810c5e8c40c6a2ef5b79ff583ea8ca
misebox <misebox@gmail.com>
2024-01-20 14:21:13 +0900

    WIP

--- src/flipid.ts
+++ src/flipid.ts
@@ -1,18 +1,25 @@
 import { Buffer } from "node:buffer";
 import { encrypt } from "./simple-cipher";
 
-export class FlipIDGenerator {
-  key: string;
-  constructor(key: string) {
-    this.key = key;
+// Error for when the block is larger than byteSize
+class BlockTooLargeError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "BlockTooLargeError";
   }
+}
 
-  encodeNumber(n: number, seed: string): string {
-    if (n < 0 || n > 2 ** 32 - 1) {
-      throw new Error("Number must be between 0 and 2^32-1");
+export class FlipIDGenerator {
+  constructor(private key: string, private byteSize: number = 4) {}
+
+  encodeNumber(n: number | bigint, seed: string): string {
+    const buf = Buffer.from(n.toString(16), "hex");
+    if (buf.length > this.byteSize) {
+      throw new Error();
     }
+    const block = Buffer.alloc(this.byteSize);
+    buf.copy(block, this.byteSize - buf.length);
     let hex = ("00000000" + n.toString(16)).slice(-8);
-    const block = Buffer.from(hex, "hex");
     const key = Buffer.from(
       this.key
         .repeat(Math.ceil(block.length / this.key.length))
